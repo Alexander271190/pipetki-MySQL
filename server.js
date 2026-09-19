55f12aa18fccde7329c48082438f017f03eb6c03
alexander271190 <sasha271190@mail.ru>
2026-09-19 17:35:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: '*', credentials: true }));
+app.use(cors({ origin: true, credentials: false }));
 app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 
@@ -68,6 +68,6 @@ const db = require('./db');
 
   app.listen(PORT, () => {
     console.log(`🚀 Server on http://0.0.0.0:${PORT}`);
-    console.log(`👤 admin/admin, senior/senior, user/user`);
+    console.log(`👤 Пользователи по умолчанию созданы`);
   });
 })();
