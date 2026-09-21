833efaf5bf014f1b7cd2004a869cbc7ad2e446bd
alexander271190 <sasha271190@mail.ru>
2026-09-21 07:48:39 +0700

    Create passwordPolicy.js

---
+++ middleware/passwordPolicy.js
@@ -0,0 +1,21 @@
+// ============================================================
+// ПОЛИТИКА ПАРОЛЯ
+// ============================================================
+const MIN_LENGTH = 8;
+
+function validatePassword(password) {
+  const errors = [];
+
+  if (!password || typeof password !== 'string') {
+    return { ok: false, errors: ['Пароль не задан'] };
+  }
+  if (password.length < MIN_LENGTH)   errors.push(`Минимум ${MIN_LENGTH} символов`);
+  if (!/[a-z]/.test(password))        errors.push('Хотя бы одна строчная буква (a-z)');
+  if (!/[A-Z]/.test(password))        errors.push('Хотя бы одна заглавная буква (A-Z)');
+  if (!/[0-9]/.test(password))        errors.push('Хотя бы одна цифра (0-9)');
+  if (!/[^A-Za-z0-9]/.test(password)) errors.push('Хотя бы один спецсимвол (!@#$%^&* и т.п.)');
+
+  return { ok: errors.length === 0, errors };
+}
+
+module.exports = { validatePassword, MIN_LENGTH };
